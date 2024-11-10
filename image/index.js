fef65b7ff765813b17125db23a12aee8a3dfd617
Noikstrax <noikstrax@tuta.io>
2024-11-10 21:35:29 +0300

    Update index.js

--- image/index.js
+++ image/index.js
@@ -1,6 +1,6 @@
 const image = (param) => {
     return `<figure class="w-content content-block-image flex justify-center items-center">
-    <img class="max-h-600 object-cover w-full rounded-lg" src='${param.file.url}' alt='${param.caption}'/>
+    <img class="object-cover mx-auto w-10/12" src='${param.file.url}' alt='${param.caption}'/>
     <figcaption>${param.caption}</figcaption>
     </figure>`
 }
