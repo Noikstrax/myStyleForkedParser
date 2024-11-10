80ebd96348318909b2b2dc48f1564ce3a5a88b74
Noikstrax <noikstrax@tuta.io>
2024-11-10 21:25:19 +0300

    Update index.js

--- embed/index.js
+++ embed/index.js
@@ -1,7 +1,7 @@
 let val = ''
 const embed = (param) => {
     val = ''
-    val += `<iframe style="border: none;" width='100%' height='${param.height + 100}' src='${param.embed}'></iframe>`
+    val += `<iframe style="border: none;" width='100%' height='${param.height + 100}' src='${param.embed}' class='mt-4'></iframe>`
     return val
 }
 
